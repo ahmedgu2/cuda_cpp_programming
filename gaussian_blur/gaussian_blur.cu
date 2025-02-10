e34718d713d856efee6c5b947b5e35d03de741cb
Ahmed Gueddiche <ahmed.gueddiche2@gmail.com>
2025-02-10 14:16:17 +0100

    seperated cuda from cpu code

--- gaussian_blur/gaussian_blur.cu
+++ gaussian_blur/gaussian_blur.cu
@@ -1,48 +1,22 @@
 #include <iostream>
-#include <random>
 #include <cuda_runtime.h>
 #include <cmath>
 
 // GPU version
 __device__ 
-float normal_pdf(const float x, const float mean, const float std){
+float normal_pdf2D_gpu(const float x, const float y, const float mean, const float std){
     const float pi = 3.141592653589;
-    const float coeff = 1 /  (std * sqrtf(2 * pi));
-    const float exponent = - 0.5 * powf((x - mean) / std, 2.0);
+    const float coeff = 1 / (2 * pi * std * std);
+    const float exponent = - 0.5 * ((x * x + y * y) / (std * std));
     return coeff * expf(exponent);
 }
 
 __global__
 void gaussianBlur_gpu(
-    float **mat,
+    float *mat,
     const int nRows,
     const int nCols,
     const int kernelSize
 ){
 
-}
- 
-// CPU version
-float normal_pdf(const float x, const float mean, const float std){
-    const float pi = 3.141592653589;
-    const float coeff = 1 /  (std * std::sqrt(2 * pi));
-    const float exponent = - 0.5 * std::pow((x - mean) / std, 2.0);
-    return coeff * std::exp(exponent);
-}
-
-
-void gaussianBlur_cpu(
-    float **mat,
-    const int nRows,
-    const int nCols,
-    const int kernelSize
-){
-    float **kernel;
-    std::normal_distribution<float> gaussian(0, 1.f);
-    
-}
-
-int main(){
-
-    return 0;
 }
