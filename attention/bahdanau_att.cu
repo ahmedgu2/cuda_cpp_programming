0bd3060c3083ecc3bd90d9cb8e100e02f77fec8f
Ahmed Gueddiche <ahmed.gueddiche2@gmail.com>
2025-02-27 23:55:00 +0100

    concat and tanh implementation

--- attention/bahdanau_att.cu
+++ attention/bahdanau_att.cu
@@ -40,9 +40,9 @@ void printVector(float *vector, const int length){
 
 /**************** Luong attention implementation ************* */
 /**
- * Luong model is as follows:
- * - encoder: unidirectional (simple).
- * - attention score: biliniaire function, score(h_t, s_k) = h^T_{t} x W x s_k
+ * Badhanau model is as follows:
+ * - encoder: bi-directional.
+ * - attention score: biliniaire function, score(h_t, s_k) = W^T_2 x tanh(W_1 [h_{t-1}, s_k])
  * - attention applied between RNN decoder at step t and prediction for this step.
  */
 __global__
@@ -80,6 +80,128 @@ void matmul(float *mat1, uint32_t nRows1, uint32_t nCols1, float *mat2, uint32_t
 }
 
 __global__
-void bahdanauAttScore_gpu(){
+void concat(float *x, const int len_x, float *y, const int len_y, float *output, float len_out){
+    for(int indx = threadIdx.x + blockDim.x * blockIdx.x; indx < len_out; indx += gridDim.x * blockDim.x){
+        if(indx < len_x)
+            output[indx] = x[indx];
+        if(indx >= len_x)
+            output[indx] = y[indx - len_x];
+    }
+}
+
+__global__
+void tanh_(float *mat, const int nRows, const int nCols, float *output){
+    int row = threadIdx.y + blockDim.y * blockIdx.y;
+    int col = threadIdx.x + blockDim.x * blockIdx.x;
+
+    if(row < nRows && col < nCols){
+        output[row * nCols + col] = tanhf(mat[row * nCols + col]);
+    }
+}
+
+
+void bahdanauAttScore_gpu(
+    float *w_2_T,
+    float *W_1,
+    float *h,
+    float *S,
+    const int d,
+    const int m
+){
+    /**
+     * 
+     */
+
+    float *d_w_2_T, *d_W_1, *d_h, *d_S, *d_concat, *d_matmul;
+
+        // Allocate memory on GPU
+    CUDA_CHECK_ERROR(cudaMalloc((void**)&d_w_2_T, d * sizeof(float)));       // Shape: (1, d)
+    CUDA_CHECK_ERROR(cudaMalloc((void**)&d_W_1, d * 2 * d * sizeof(float)));    // Shape: (d, 2d)
+    CUDA_CHECK_ERROR(cudaMalloc((void**)&d_h, d * sizeof(float)));         // Shape: (d, 1)
+    CUDA_CHECK_ERROR(cudaMalloc((void**)&d_S, d * sizeof(float)));     // Shape: (d, 1)
+    CUDA_CHECK_ERROR(cudaMalloc((void**)&d_matmul, d * sizeof(float)));     // Shape: (d, 1)
+    CUDA_CHECK_ERROR(cudaMalloc((void**)&d_concat, 2 * d * sizeof(float)));     // Shape: (2d, 1)
+
+    // Copy memory from host to device
+    CUDA_CHECK_ERROR(cudaMemcpy(d_w_2_T, w_2_T, d * sizeof(float), cudaMemcpyHostToDevice));
+    CUDA_CHECK_ERROR(cudaMemcpy(d_W_1, W_1, d * d * sizeof(float), cudaMemcpyHostToDevice));
+    CUDA_CHECK_ERROR(cudaMemcpy(d_h, h, d * sizeof(float), cudaMemcpyHostToDevice));
+    CUDA_CHECK_ERROR(cudaMemcpy(d_S, S, d * m * sizeof(float), cudaMemcpyHostToDevice));
+
+    // TODO: Implement kernel execution
+    int BLOCK_DIM = 256;
+    dim3 gridSize((d + BLOCK_DIM - 1) / BLOCK_DIM);
+    concat<<<gridSize, BLOCK_DIM>>>(d_h, d, d_S, d, d_concat, 2 * d);
+    CUDA_KERNEL_CHECK_ERROR();
+
+    dim3 numThreads(BLOCK_DIM, BLOCK_DIM);
+    matmul<<<gridSize, numThreads>>>(d_W_1, 2 * d, d, d_concat, 2 * d, 1, d_matmul);
+    CUDA_KERNEL_CHECK_ERROR();
+    // dim3 numThreads(BLOCK_DIM, BLOCK_DIM);
+
+    tanh_<<<gridSize, numThreads>>>(d_matmul, d, 1, d_matmul);
+    CUDA_KERNEL_CHECK_ERROR();
     
+    // Free allocated memory (should be done after kernel execution)
+    CUDA_CHECK_ERROR(cudaFree(d_w_2_T));
+    CUDA_CHECK_ERROR(cudaFree(d_W_1));
+    CUDA_CHECK_ERROR(cudaFree(d_h));
+    CUDA_CHECK_ERROR(cudaFree(d_S));
+    CUDA_CHECK_ERROR(cudaFree(d_concat));
+    CUDA_CHECK_ERROR(cudaFree(d_matmul));
+}
+
+/******************** Unit testing ******************* */
+void concat_cpu(float *x, const int len_x, float *y, const int len_y, float *output, const int len_out){
+    for(int i = 0; i < len_out; i++){
+        if(i < len_x)
+            output[i] = x[i];
+        else
+            output[i] = y[i - len_x];
+    }
+}
+
+void test_concat(){
+    const int len_x = 1e5, len_y = 1024;
+    const int len_out = len_x + len_y;
+    float x[len_x], y[len_y];
+    float *output_cpu = new float[len_out];
+    float *output_gpu = new float[len_out];
+
+    initVector(x, len_x);
+    initVector(y, len_y);
+
+    float *d_x, *d_y, *d_output;
+
+    CUDA_CHECK_ERROR(cudaMalloc(&d_x, len_x * sizeof(float)));
+    CUDA_CHECK_ERROR(cudaMalloc(&d_y, len_y * sizeof(float)));
+    CUDA_CHECK_ERROR(cudaMalloc(&d_output, len_out * sizeof(float)));
+    
+    CUDA_CHECK_ERROR(cudaMemcpy(d_x, x, len_x * sizeof(float), cudaMemcpyHostToDevice));
+    CUDA_CHECK_ERROR(cudaMemcpy(d_y, y, len_y * sizeof(float), cudaMemcpyHostToDevice));
+
+    int BLOCK_DIM = 256;
+    int gridSize = (len_out + BLOCK_DIM - 1) / BLOCK_DIM;
+    concat<<<BLOCK_DIM, gridSize>>>(d_x, len_x, d_y, len_y, d_output, len_out);
+    CUDA_KERNEL_CHECK_ERROR();
+
+    CUDA_CHECK_ERROR(cudaMemcpy(output_gpu, d_output, len_out * sizeof(float), cudaMemcpyDeviceToHost));
+
+    concat_cpu(x, len_x, y, len_y, output_cpu, len_out);
+
+    for(int i = 0; i < len_out; ++i){
+        if(output_cpu[i] != output_gpu[i]){
+            std::cerr << "Mismatch at index " << i << ". Expected " << output_cpu[i] << ", Found " << output_gpu[i] << std::endl;
+            std::cerr << "\033[31mTEST FAILED\033[0m" << std::endl;
+            exit(EXIT_FAILURE);
+        }
+    }
+    std::cout << "\033[32mTEST PASSED!\033[0m" << std::endl;
+
+    delete[] output_cpu;
+    delete[] output_gpu;
+}
+
+int main(){
+    test_concat();
 }
