b394cb85970e31433715db7c19242df79035199d
Ahmed Gueddiche <ahmed.gueddiche2@gmail.com>
2025-03-05 01:39:29 +0100

    utility cuda functions

--- attention/src/cuda_utils.cu
+++ attention/src/cuda_utils.cu
@@ -1,9 +1,13 @@
 #include "cuda_macros.cuh"
 
 void allocateCudaMemory(float **ptr, size_t size){
-    CUDA_CHECK_ERROR(cudaMalloc(&ptr, size * sizeof(float)));
+    CUDA_CHECK_ERROR(cudaMalloc(ptr, size * sizeof(float)));
 }
 
 void freeCudaMemory(float *ptr){
     CUDA_CHECK_ERROR(cudaFree(ptr));
+}
+
+void copyToCuda(float *dst_ptr, float *src_ptr, size_t size){
+    CUDA_CHECK_ERROR(cudaMemcpy(dst_ptr, src_ptr, size * sizeof(float), cudaMemcpyHostToDevice));
 }
