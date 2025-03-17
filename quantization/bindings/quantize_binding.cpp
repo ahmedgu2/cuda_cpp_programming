4078c58a39eafa9f944261617c3a251b17a8c9c6
Ahmed Gueddiche <ahmed.gueddiche2@gmail.com>
2025-03-17 19:30:02 -0400

    update row-wise quant to take only input on device and float16

--- quantization/bindings/quantize_binding.cpp
+++ quantization/bindings/quantize_binding.cpp
@@ -18,17 +18,41 @@ torch::Tensor symmetric_quantization_gpu(torch::Tensor& array){
     return q_array;
 }
 
-torch::Tensor row_wise_quantization_gpu(torch::Tensor& tensor){
-    auto q_tensor = torch::zeros_like(tensor, torch::kInt8);
+// torch::Tensor row_wise_quantization_gpu(torch::Tensor& tensor){
+//     auto q_tensor = torch::zeros_like(tensor, torch::kInt8);
 
-    int nRows = tensor.size(0);
-    int nCols = tensor.size(1);
+//     int nRows = tensor.size(0);
+//     int nCols = tensor.size(1);
 
-    __half *tensor_ptr = reinterpret_cast<__half*>(tensor.data_ptr<at::Half>());
-    int8_t *q_tensor_ptr = q_tensor.data_ptr<int8_t>();
+//     __half *tensor_ptr = reinterpret_cast<__half*>(tensor.data_ptr<at::Half>());
+//     int8_t *q_tensor_ptr = q_tensor.data_ptr<int8_t>();
 
-    rowWiseQuant8bits_gpu(tensor_ptr, nRows, nCols, q_tensor_ptr);
-    return q_tensor;
+//     rowWiseQuant8bits_gpu(tensor_ptr, nRows, nCols, q_tensor_ptr);
+//     return q_tensor;
+// }
+
+torch::Tensor row_wise_quantization_gpu(torch::Tensor& d_tensor){
+    if(d_tensor.dtype() != torch::kFloat16){
+        std::cerr << "Tensor must have float16 dtype!" << std::endl;
+        return torch::zeros(1);
+    }
+    if(!d_tensor.device().is_cuda()){
+        std::cerr << "Tensor must be on device!" << std::endl;
+        return torch::zeros(1);
+    }
+
+    int nRows = d_tensor.size(0);
+    int nCols = d_tensor.size(1);
+
+    torch::Tensor d_rowsScale = torch::empty(nRows, torch::TensorOptions().dtype(torch::kFloat16).device(torch::kCUDA));
+    torch::Tensor d_q_tensor = torch::empty({nRows, nCols}, torch::TensorOptions().dtype(torch::kInt8).device(torch::kCUDA));
+
+    __half *d_tensor_ptr = reinterpret_cast<__half*>(d_tensor.data_ptr<at::Half>());
+    __half *d_rowsScale_ptr = reinterpret_cast<__half*>(d_rowsScale.data_ptr<at::Half>());
+    int8_t *d_q_tensor_ptr = d_q_tensor.data_ptr<int8_t>();
+
+    rowWiseQuant8bits_gpu2(d_tensor_ptr, nRows, nCols, d_q_tensor_ptr, d_rowsScale_ptr);
+    return d_q_tensor;
 }
 
 torch::Tensor column_wise_quantization_gpu(torch::Tensor& tensor){
