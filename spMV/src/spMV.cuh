7de9cf18c9272f1847d4fb9f678a155ee8c5046a
Ahmed Gueddiche <ahmed.gueddiche2@gmail.com>
2025-03-21 18:29:51 -0400

    CSRMatrixGPU implementation

--- spMV/src/spMV.cuh
+++ spMV/src/spMV.cuh
@@ -2,3 +2,4 @@
 #include "COOMatrixGPU.cuh"
 
 void spmvCoo_gpu(COOMatrixGPU& mat, size_t nRows, size_t nCols, float *x, float *y);
+void spmvCSR_gpu(CSRMatrixGPU& mat, size_t nRows, size_t nCols, float *x, float *y);
--- spMV/test/test.cpp
+++ spMV/test/test.cpp
@@ -1,9 +1,10 @@
 #include "COOMatrixGPU.cuh"
+#include "CSRMatrixGPU.cuh"
 #include "utils.h"
 #include "spMV.cuh"
 #include <cstring>
 
-int main(){
+void test_spmv_coo(){
     size_t nRows = 10, nCols = 10;
     float *mat = new float[nRows * nCols];
     float *x = new float[nCols];
@@ -26,4 +27,34 @@ int main(){
     delete[] y_cpu;
     delete[] y_gpu;
     delete[] mat;
+}
+
+void test_spmv_csr(){
+    size_t nRows = 1024, nCols = 1024;
+    float *mat = new float[nRows * nCols];
+    float *x = new float[nCols];
+    float *y_cpu = new float[nRows];
+    float *y_gpu = new float[nRows];
+
+    initVector(mat, nRows * nCols);
+    initVector(x, nCols);
+    memset(y_cpu, 0, nRows * sizeof(float));
+    memset(y_gpu, 0, nRows * sizeof(float));
+    
+
+    matmul_cpu(mat, nRows, nCols, x, nCols, 1, y_cpu);
+    CSRMatrixGPU matCSR(mat, nRows, nCols);
+    spmvCSR_gpu(matCSR, nRows, nCols, x, y_gpu);
+    
+    compareArrays(y_cpu, y_gpu, nRows);
+
+    delete[] x;
+    delete[] y_cpu;
+    delete[] y_gpu;
+    delete[] mat;
+}
+
+int main(){
+    // test_spmv_coo();
+    test_spmv_csr();
 }
